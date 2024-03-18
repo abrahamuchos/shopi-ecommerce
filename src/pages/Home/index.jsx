6ed2b1679d9b3b1a8fc361a7614cf1dd984b1191
abrahamuchos <j.abraham29@gmail.com>
2024-03-18 16:13:46 -0400

    Add cards structure

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import Layout from "../../componets/Layout/index.jsx";
+import Card from "../../componets/Card/index.jsx";
 
 export default function Home() {
   return (
+    <>
       <h1>Home</h1>
+      <Card/>
+
+    </>
   );
 }
